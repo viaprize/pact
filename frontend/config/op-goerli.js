a4078c1c6b578af9cfedf504f6d352430c7c6c95
vibhurajeev <vibhurajeev@gmail.com>
2023-04-16 13:54:43 +0200

    feat(pact factory): create pact

--- frontend/config/op-goerli.js
+++ frontend/config/op-goerli.js
@@ -1,6 +1,7 @@
 export default {
   provider: "https://goerli.optimism.io",
   chainId: 420,
+  chainName: "Optimism Goerli",
   scanUrl: "https://goerli-explorer.optimism.io/",
   contracts: {
     pactFactory: "0x642a7864cBe44ED24D408Cbc38117Cfd6E6D1a95",
